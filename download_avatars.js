d3a47140671c6814f918937515a12bcc285f8ba8
Thomassky28 <haifeng3@ualberta.ca>
2018-08-07 01:00:51 +0000

    step 8

--- download_avatars.js
+++ download_avatars.js
@@ -6,21 +6,27 @@ var secrets = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN;
+      'Authorization': secrets.GITHUB_TOKEN
     }
 
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function(i){
+    console.log(i.login);
+    console.log(i.avatar_url)
+  })
+
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 });
